d129acafe5aaea01b11328afdec57013be601698
Ahmad Riady S <ariadys42@gmail.com>
2019-04-04 21:52:49 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Create from './components/create.component';
+import Index from './components/index.component';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+          <nav className="navbar" role="navigation" aria-label="main navigation">
+            <div className="container">
+              <div className="navbar-brand">
+                <a className="navbar-item" href="https://bulma.io">
+                  <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" alt="Logo" />
+                </a>
+
+                <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden="true"></span>
+                </a>
+              </div>
+              <div className="navbar-menu" id="navbarBasicExample">
+                <div className="navbar-start">
+                  <Link to={'/'} className="navbar-item">Home</Link>
+                  <Link to={'/create'} className="navbar-item">Create</Link>
+                </div>
+              </div>
+            </div>
+          </nav>
+          <section className="hero is-primary">
+            <div className="hero-body">
+              <div className="container">
+                <h1 className="title">
+                  CRUD React
+                </h1>
+                <h2 className="subtitle">
+                  Awesome CRUD
+                </h2>
+              </div>
+            </div>
+          </section>
+          <div className="container">
+            <Switch>
+                <Route exact path='/create' component={ Create } />
+                <Route path='/index' component={ Index } />
+            </Switch>
+          </div>
+      </Router>
     );
   }
 }
---
+++ src/components/create.component.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+
+export default class Create extends Component {
+	constructor(props) {
+      	super(props);
+      	this.onChangePersonName = this.onChangePersonName.bind(this);
+      	this.onChangeBusinessName = this.onChangeBusinessName.bind(this);
+      	this.onChangeGstNumber = this.onChangeGstNumber.bind(this);
+      	this.onSubmit = this.onSubmit.bind(this);
+
+      	this.state = {
+          	person_name: '',
+          	business_name: '',
+          	business_gst_number:''
+      	}
+    }
+    onChangePersonName(e) {
+	    this.setState({
+	      person_name: e.target.value
+	    });
+  	}
+  	onChangeBusinessName(e) {
+	    this.setState({
+	      business_name: e.target.value
+	    });
+  	}
+  	onChangeGstNumber(e) {
+	    this.setState({
+	      business_gst_number: e.target.value
+	    });
+  	}
+  	onSubmit(e) {
+	    e.preventDefault();
+	    console.log(`The values are ${this.state.person_name}, ${this.state.business_name}, and ${this.state.business_gst_number}`)
+	    this.setState({
+	      person_name: '',
+	      business_name: '',
+	      business_gst_number: ''
+	    })
+  	}
+    render() {
+        return (
+	        <section class="section">
+	        	<div className="columns">
+	  				<div className="column is-3">
+	  					<form onSubmit={this.onSubmit}>
+				           <div className="field">
+				           	<label className="label">Person Name</label>
+							  <div className="control has-icons-left">
+							    <input className="input" type="text" required value={this.state.person_name}
+                        		onChange={this.onChangePersonName}/>
+							    <span className="icon is-small is-left">
+							      <i className="fas fa-envelope"></i>
+							    </span>
+							  </div>
+							</div>
+							<div className="field">
+							<label className="label">Business Name</label>
+							  <div className="control has-icons-left">
+							    <input className="input" type="text" required value={this.state.business_name}
+                        		onChange={this.onChangeBusinessName} />
+							    <span className="icon is-small is-left">
+							      <i className="fas fa-lock"></i>
+							    </span>
+							  </div>
+							</div>
+							<div className="field">
+							<label className="label">GST Number</label>
+							  <div className="control has-icons-left">
+							    <input className="input" type="text" required value={this.state.business_gst_number}
+                       			onChange={this.onChangeGstNumber} />
+							    <span className="icon is-small is-left">
+							      <i className="fas fa-lock"></i>
+							    </span>
+							  </div>
+							</div>
+							<div className="field is-grouped">
+							  <div className="control">
+							    <button type="submit" className="button is-link">Submit</button>
+							  </div>
+							  <div className="control">
+							    <button className="button is-light">Cancel</button>
+							  </div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</section>
+        )
+    }
+}
